a8cee98a3ea78aec20e5159db248e39a0d2aa1f4
DBM <diegobarmor42@gmail.com>
2026-02-23 22:38:56 +0100

    pip project template now considers __main__ usage

--- copy_template.py
+++ copy_template.py
@@ -14,9 +14,18 @@ def _replace_all(path: Path, old: str, new: str):
 def template_c_raylib(variant: str = ""):
     path_template = ROOT / f"c/raylib_project{variant}"
     shutil.copytree(path_template, PATH_NEW_PROJECT)
-    _replace_all(PATH_NEW_PROJECT / ".gitignore",   "raylib_project", PATH_NEW_PROJECT.name.replace(" ", "\\ "))
-    _replace_all(PATH_NEW_PROJECT / "README.md",    "Raylib Project", PATH_NEW_PROJECT.name)
-    _replace_all(PATH_NEW_PROJECT / "src/params.c", "Raylib Project", PATH_NEW_PROJECT.name)
+    _replace_all(PATH_NEW_PROJECT / ".gitignore",   "raylib_project", NAME_NEW_PROJECT)
+    _replace_all(PATH_NEW_PROJECT / "README.md",    "Raylib Project", NAME_NEW_PROJECT)
+    _replace_all(PATH_NEW_PROJECT / "src/params.c", "Raylib Project", NAME_NEW_PROJECT)
+
+
+# ------------------------------------------------------------------------------
+def template_py_pip():
+    path_template = ROOT / "python/pip_project"
+    shutil.copytree(path_template, PATH_NEW_PROJECT)
+    shutil.move(PATH_NEW_PROJECT / "pip_project", PATH_NEW_PROJECT / NAME_NEW_PROJECT )
+    for path_file in ["environment.yml", "install.sh", "README.md", "setup.py", "upload.sh"]:
+        _replace_all(PATH_NEW_PROJECT / path_file, "pip_project", NAME_NEW_PROJECT)
 
 
 # ------------------------------------------------------------------------------
@@ -26,6 +35,7 @@ def main():
         case "c_raylib":      template_c_raylib()
         case "c_raylib_mod":  template_c_raylib("_modular")
         case "c_raylib_wasm": template_c_raylib("_wasm")
+        case "py_pip":        template_py_pip()
         case _: raise ValueError(f"Template '{NAME_TEMPLATE}' is not available. Available templates: {available_templates}")
 
 
@@ -34,6 +44,9 @@ if __name__ == "__main__":
     NAME_TEMPLATE = sys.argv[1]
     PATH_NEW_PROJECT = Path(sys.argv[2])
     ROOT = Path(__file__).parent
+
+    NAME_NEW_PROJECT = PATH_NEW_PROJECT.name.replace(' ', '_') # ensure there are no spaces in the name...
+    PATH_NEW_PROJECT = PATH_NEW_PROJECT.parent / NAME_NEW_PROJECT
     main()
 
 
--- python/pip_project/setup.py
+++ python/pip_project/setup.py
@@ -1,9 +1,16 @@
 from setuptools import setup, find_packages
+from pathlib import Path
+
+def read_requirements(path = "requirements.txt"):
+    p = Path(path)
+    if not p.exists(): return []
+    stripped_lines = (ln.strip() for ln in p.read_text().strip().splitlines())
+    return [ln for ln in stripped_lines if ln and not ln.startswith("#")]
 
 setup(
     name="pip_project",
     version="0.1.0",
-    description="WIP_PLACEHHOLDER",
+    description="Package description",
     keywords="",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
@@ -15,7 +22,12 @@ setup(
     package_data={ # optional
         "pip_project": ["config.ini", "_data/*"],
     },
-    install_requires=[],
+    install_requires=read_requirements(), # replace with install_requires=[] if there are no requirements
+    entry_points={ # comment out if package is intended to be used only via imports
+        "console_scripts": [
+            "pip_project=pip_project.__main__:main",
+        ],
+    },
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
